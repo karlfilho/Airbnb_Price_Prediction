81b92e8455c2593b952dc201d044dda3a62d1e9b
Karl Filho <karlfilho@gmail.com>
2025-01-18 05:34:54 -0300

    Reorganize data loading functions
    - Create dataLoader class into data_loader.py
    - Change loader functions into dataLoader class

--- src/data/data_loader.py
+++ src/data/data_loader.py
@@ -1,14 +1,30 @@
-import pandas as pd
-import json
+"""Data loader code"""
 
-class DataProcessor:
-    def __init__(self):
-        pass
+import pandas as pd
+
+
+class DataLoader:
+    def __init__(self, frontpage_path: str, rooms_path: str):
+        self.frontpage_path = frontpage_path
+        self.rooms_path = rooms_path
+        self.frontpage_data = None
+        self.rooms_data = None
+        self.merged_data = None
     
-    def load_data(self, file_path: str) -> pd.DataFrame:
-        # Carregar e processar dados
-        pass
+    def load_data_frontpage(self) -> pd.DataFrame:
+        """Load data from the 'frontpage' json file"""
+        frontpage_data = pd.read_json(self.frontpage_path, encoding='utf-8')
+        frontpage_data = frontpage_data.drop(columns= ["Keyword", "Host", "roomName", "roomRating", "roomReviewcount"], inplace=True)
+        return frontpage_data
     
-    def preprocess_features(self, data: pd.DataFrame) -> pd.DataFrame:
-        # Preparar features para o modelo
-        pass 
+    def load_data_rooms(self) -> pd.DataFrame:
+        """Load data from the 'rooms' json file"""
+        rooms_data = pd.read_json(self.rooms_path, encoding='utf-8')
+        rooms_data = rooms_data.drop(columns= ["Title", "Location", "Number_of_Guests", "Number_of_Bedrooms", "Number_of_Beds", "Number_of_Bath", "Price", "Sleeping_Arrangements", "Hosted_by", "Response_Rate", "Image_1", "Image_2", "Image_3", "Current_Time"], inplace=True)
+        return rooms_data
+    
+    def merge_data(self) -> pd.DataFrame:
+        """Merge the 'frontpage' and 'rooms' dataframes"""
+        merged_data = pd.merge(self.frontpage_data, self.rooms_data, left_on='roomURL', right_on='Page_URL')
+        merged_data = merged_data.drop(columns=["Page_URL"])    # keeping the url for future cleaning
+        return merged_data
--- src/data/data_preprocessing.py
+++ src/data/data_preprocessing.py
@@ -1,5 +1,7 @@
+"""Data preprocessing code"""
+
 import pandas as pd
-import numpy as np
+
 
 def load_data_frontpage(frontpage_path: str) -> pd.DataFrame:
     """Load data from the 'frontpage' json file"""
@@ -12,3 +14,10 @@ def load_data_rooms(rooms_path: str) -> pd.DataFrame:
     rooms_df = pd.read_json(rooms_path, encoding='utf-8')
     rooms_df = rooms_df.drop(columns= ["Title", "Location", "Number_of_Guests", "Number_of_Bedrooms", "Number_of_Beds", "Number_of_Bath", "Price", "Sleeping_Arrangements", "Hosted_by", "Response_Rate", "Image_1", "Image_2", "Image_3", "Current_Time"], inplace=True)
     return rooms_df
+
+def merge_data(frontpage_df: pd.DataFrame, rooms_df: pd.DataFrame) -> pd.DataFrame:
+    """Merge the 'frontpage' and 'rooms' dataframes"""
+    merged_df = pd.merge(frontpage_df, rooms_df, left_on='roomURL', right_on='Page_URL')
+    merged_df = merged_df.drop(columns=["Page_URL"]) # keeping the url for future cleaning
+    return merged_df
+
